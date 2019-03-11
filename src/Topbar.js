b43098770bc266fa60474a557744b232fcf30c84
Schwin Pinkoh <schwin.pi@thaigertec.com>
2019-03-11 14:57:38 +0700

    tryout reactstrap

--- src/Topbar.js
+++ src/Topbar.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import './Topbar.css'
+import { Container, Row, Col, Button } from 'reactstrap';
 
 export default class Topbar extends Component {
     render() {
         return (
-            <div>
-
-                
-            </div>
+            <Button color="danger">Danger!</Button>
+        // <Container>
+        // <Row>
+        //   <Col>.col</Col>
+        // </Row>
+        // <Row>
+        //   <Col>.col</Col>
+        //   <Col>.col</Col>
+        //   <Col>.col</Col>
+        //   <Col>.col</Col>
+        // </Row>
+        // </Container>
         )
     }
 }
